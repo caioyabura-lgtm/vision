433b38038282f9ee1a91d4252f8a28501c205800
caioyabura-lgtm <caioyabura@gmail.com>
2026-04-19 10:40:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-document.body.style.background = 'red';
+document.body.style.margin = '0';
+document.body.style.background = '#111';
+
+const img = document.createElement('img');
+img.src = './logo.png';
+img.style.width = '300px';
+img.style.position = 'fixed';
+img.style.left = '50%';
+img.style.top = '50%';
+img.style.transform = 'translate(-50%, -50%)';
+document.body.appendChild(img);
 
 const msg = document.createElement('div');
-msg.textContent = 'GitHub Pages atualizou';
+msg.textContent = 'Teste logo.png';
 msg.style.position = 'fixed';
-msg.style.top = '30px';
-msg.style.left = '30px';
+msg.style.top = '20px';
+msg.style.left = '20px';
 msg.style.color = 'white';
-msg.style.fontSize = '32px';
-msg.style.zIndex = '9999';
+msg.style.fontSize = '24px';
 document.body.appendChild(msg);
